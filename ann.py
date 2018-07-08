09af0f1687a6510f39f0996cf342f59075c14942
Ishantha Udara <ishanthaudara96@gmail.com>
2018-07-08 06:55:27 +0530

    updated

--- ann.py
+++ ann.py
@@ -9,15 +9,17 @@
 # Installing Keras
 # pip install --upgrade keras
 
-# Part 1 - Data Preprocessing
+
+
+# Importing the libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Churn_Modelling.csv')
-X = dataset.iloc[:, [2, 3]].values
-y = dataset.iloc[:, 4].values
+X = dataset.iloc[:, 3:13].values
+y = dataset.iloc[:, 13].values
 
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
